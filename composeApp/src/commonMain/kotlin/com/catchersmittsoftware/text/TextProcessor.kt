8d2d9b4d994f119d21dbae050443438becc1074f
Alonso del Arte <alonso.delarte@gmail.com>
2026-04-22 23:19:03 -0400

    Function stubs

--- composeApp/src/commonMain/kotlin/com/catchersmittsoftware/text/TextProcessor.kt
+++ composeApp/src/commonMain/kotlin/com/catchersmittsoftware/text/TextProcessor.kt
@@ -1,4 +1,20 @@
 package com.catchersmittsoftware.text
 
 object TextProcessor {
+
+    // TODO: Write tests for this
+    fun isHighSurrogate(ch: Char): Boolean {
+        return false
+    }
+
+    // TODO: Write tests for this
+    fun isLowSurrogate(ch: Char): Boolean {
+        return false
+    }
+
+    // TODO: Write tests for this
+    fun isSurrogatePair(s: String): Boolean {
+        return false
+    }
+
 }
