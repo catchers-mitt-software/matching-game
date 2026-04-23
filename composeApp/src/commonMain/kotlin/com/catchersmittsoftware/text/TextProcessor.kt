5abdc6252048b15c7c3a7fee16e708ed07b7b65c
Alonso del Arte <alonso.delarte@gmail.com>
2026-04-22 23:01:09 -0400

    New package

---
+++ composeApp/src/commonMain/kotlin/com/catchersmittsoftware/text/TextProcessor.kt
@@ -0,0 +1,4 @@
+package com.catchersmittsoftware.text
+
+class TextProcessor {
+}
---
+++ composeApp/src/commonTest/kotlin/com/catchersmittsoftware/text/TextProcessorTest.kt
@@ -0,0 +1,7 @@
+package com.catchersmittsoftware.text
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class TextProcessorTest {
+}
