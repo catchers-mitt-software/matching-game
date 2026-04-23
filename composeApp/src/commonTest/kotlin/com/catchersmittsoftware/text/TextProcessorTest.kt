ffeb761aabfd0ecd9bcaad97d48dd7e9e19e1035
Alonso del Arte <alonso.delarte@gmail.com>
2026-04-22 23:29:30 -0400

    Failing isHighSurrogate( ) test

--- composeApp/src/commonTest/kotlin/com/catchersmittsoftware/text/TextProcessorTest.kt
+++ composeApp/src/commonTest/kotlin/com/catchersmittsoftware/text/TextProcessorTest.kt
@@ -1,7 +1,17 @@
 package com.catchersmittsoftware.text
 
 import kotlin.test.Test
-import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class TextProcessorTest {
+
+    @Test
+    fun testIsHighSurrogate() {
+        val num = (0xD800 .. 0xDBFF).random()
+        val ch: Char = num.toChar()
+        val message =
+            "Character $ch (${num.toHexString()}) should be high surrogate"
+        assertTrue(TextProcessor.isHighSurrogate(ch), message)
+    }
+
 }
